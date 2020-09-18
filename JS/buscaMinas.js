09ba2e32761f4d5f393b06e96f9f7c59a4cf9f1a
Federico Lamas <federicolamas95@gmail.com>
2020-09-18 17:58:32 -0300

    mapa en clases

--- JS/buscaMinas.js
+++ JS/buscaMinas.js
@@ -9,13 +9,13 @@ class Position {
 }
 
 class Casilla {
-    contructor(x, y) {
+    constructor(x, y) {
         this.position = new Position(x, y);
         this.html = document.createElement('div');
-        html.className = 'casilla';
-        html.addEventListener('click', () => {
-            html.style.backgroundColor = 'white';
-            html.innerText = position.toString();
+        this.html.className = 'casilla';
+        this.html.addEventListener('click', () => {
+            this.html.style.backgroundColor = 'white';
+            this.html.innerText = this.position.toString();
         });
         
     }
@@ -25,13 +25,12 @@ class Casilla {
 class Mapa {
     constructor(ancho, alto) {
         this.mapa = [];
-        this.llenarMapa();
+        this.llenarMapa(ancho, alto);
         this.html = document.createElement('div');
         this.html.id = 'map';
-
+        this.llenarhtml();
+        this.html.style.gridTemplate = 'repeat('+ alto +', 1fr)/repeat('+ ancho +', 1fr)';
     }
-
-    
 
     llenarMapa(ancho, alto) {
         for(let y = 0; y < alto; y++) {
@@ -39,8 +38,27 @@ class Mapa {
             for(let x = 0; x < ancho; x++) {
                 thisRow.push(new Position(x, y));
             }
-            mapa.push(thisRow);
+            this.mapa.push(thisRow);
         }
+    }
+
+    llenarhtml() {
+        this.mapa.forEach(row =>  {
+            row.forEach(position => {
+                let casilla = new Casilla(position.x, position.y);
+                this.html.appendChild(casilla.html);
+                /*
+                let casilla = document.createElement('div');
+                casilla.className = 'casilla';
+                casilla.addEventListener('click', () => {
+                    casilla.style.backgroundColor = 'white';
+                    casilla.innerText = position.toString();
+                });
+                this.html.appendChild(casilla);
+                //*/
+            });
+            
+        });
     }
 }
 
@@ -49,32 +67,7 @@ link.rel = 'stylesheet';
 link.href = 'Styles.css';
 document.getElementsByTagName('head')[0].appendChild(link);
 const buscaMinas = document.getElementById("buscaMinas");
-const mapaHTML = document.createElement('div');
-mapaHTML.id = 'map';
-buscaMinas.appendChild(mapaHTML);
+const mapa = new Mapa(6, 6);
+buscaMinas.appendChild(mapa.html);
 
-let ancho = 6;
-let alto = 6;
-const map = [];
-for(let y = 0; y < alto; y++) {
-    const thisRow = []
-    for(let x = 0; x < ancho; x++) {
-        thisRow.push(new Position(x, y));
-    }
-    map.push(thisRow);
-}
 
-mapaHTML.style.gridTemplate = 'repeat('+ alto +', 1fr)/repeat('+ ancho +', 1fr)';
-
-map.forEach(row =>  {
-    row.forEach(position => {
-        let casilla = document.createElement('div');
-        casilla.className = 'casilla';
-        casilla.addEventListener('click', () => {
-            casilla.style.backgroundColor = 'white';
-            casilla.innerText = position.toString();
-        });
-        mapaHTML.appendChild(casilla);
-    });
-    
-});
